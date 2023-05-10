7ca11d7e4576d5ae726b85182160e85422738f53
aShell <109769755+aShell-1@users.noreply.github.com>
2023-05-10 15:30:57 -0500

    Add files via upload

---
+++ ejt1/src/com/testeando/Tiposdedatos.java
@@ -0,0 +1,21 @@
+package com.testeando;
+
+public class Tiposdedatos {
+    public static void main(String[] args) {
+
+        int entero = 1;
+        float fdecimal = 1.26f;
+        double ddecimal = 145.566d;
+
+        String textito = "Wazaa";
+
+        boolean miente = true;
+
+        System.out.println("Tipos de datos que más recuerdo por ahora:");
+        System.out.println("int -> " + entero);
+        System.out.println("float -> " + fdecimal);
+        System.out.println("double -> " + ddecimal);
+        System.out.println("String -> " + textito);
+        System.out.println("boolean -> " + miente);
+    }
+}
